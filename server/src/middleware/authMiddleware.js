a1b22a83e16bfdf9696b0719986295d1670723d1
Charlie Taylor <dev.charlietaylor@gmail.com>
2025-02-06 11:14:44 +0000

    update middleware

--- server/src/middleware/authMiddleware.js
+++ server/src/middleware/authMiddleware.js
@@ -1,7 +1,11 @@
 import { AppError } from '../errors/index.js';
+import users from '../models/userModel.js';
 
 export default async (req, res, next) => {
-  const user = req.session.user;
+  console.log(req.session);
+  const {userId} = req.session;
+  if (!userId) return next(new AppError('Unauthorized', 401));
+  const user = await users.getWithId(userId);
   if (!user) return next(new AppError('Unauthorized', 401));
   next();
 };
