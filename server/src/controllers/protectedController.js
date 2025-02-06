9694d50298726b126cd32fda9f7e7f9d609a6520
Charlie Taylor <dev.charlietaylor@gmail.com>
2025-02-06 11:26:03 +0000

    delete old isAuth controller

--- server/src/controllers/protectedController.js
+++ server/src/controllers/protectedController.js
@@ -1,7 +1,3 @@
-export async function getIsAuth(req, res) {
-  res.json({ isAuth: true });
-};
-
 export async function getTestData(req, res) {
   res.json({ message: 'Only paid members can access this info!' });
 };
