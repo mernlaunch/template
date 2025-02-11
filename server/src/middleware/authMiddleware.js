21e50ffc2730d093224f146cd755866c8542abbe
Charlie Taylor <dev.charlietaylor@gmail.com>
2025-02-11 12:48:26 +0000

    refactor: remove console log from auth middleware

--- server/src/middleware/authMiddleware.js
+++ server/src/middleware/authMiddleware.js
@@ -2,7 +2,6 @@ import { AppError } from '../errors/index.js';
 import users from '../models/userModel.js';
 
 export default async (req, res, next) => {
-  console.log(req.session);
   const {userId} = req.session;
   if (!userId) return next(new AppError('Unauthorized', 401));
   const user = await users.getWithId(userId);
