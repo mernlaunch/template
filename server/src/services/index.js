460a8be6526a9e6e57688adc2727cee796e577dc
Charlie Taylor <dev.charlietaylor@gmail.com>
2025-02-12 15:50:19 +0000

    feat: refactor service initialization to support dynamic configuration and enable/disable options

--- server/src/services/index.js
+++ server/src/services/index.js
@@ -8,25 +8,42 @@ let services = null;
 export function initializeServices() {
   if (services) return services;
 
-  services = {
-    mailerService: new MailerService(
-      process.env.SENDGRID_API_KEY,
-      config.get('emailAddress')
-    ),
-    dbService: new DBService(
-      process.env.DB_URI
-    ),
-    paymentService: new PaymentService(
-      process.env.STRIPE_SECRET_KEY,
-      process.env.STRIPE_PRICE_ID,
-      process.env.CLIENT_URL,
-      config.get('stripe.successRoute'),
-      config.get('stripe.cancelRoute'),
-      process.env.STRIPE_WEBHOOK_SECRET
-    )
-  };
+  services = {};
+  for (const [serviceName, options] of Object.entries(config.get('services'))) {
+    if (!options.enabled) continue;
+
+    switch (serviceName) {
+      case 'mailer':
+        services.mailerService = new MailerService(
+          process.env.SENDGRID_API_KEY,
+          options.config.senderAddress,
+          options.config.templates
+        );
+        break;
+
+      case 'db':
+        services.dbService = new DBService(
+          process.env.DB_URI
+        );
+        break;
+
+      case 'payment':
+        services.paymentService = new PaymentService(
+          process.env.STRIPE_SECRET_KEY,
+          process.env.STRIPE_PRICE_ID,
+          process.env.CLIENT_URL,
+          options.config.successRoute,
+          options.config.cancelRoute,
+          process.env.STRIPE_WEBHOOK_SECRET
+        );
+        break;
+      
+      default:
+        throw new Error(`Unknown service: ${serviceName}`);
+    }
+  }
 
   return services;
-};
+}
 
 export const { mailerService, dbService, paymentService } = initializeServices();
